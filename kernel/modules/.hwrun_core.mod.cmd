3ed93faaf10574488263c483feb3b5e6506bd5d0
level0000x <level0000x@users.noreply.github.com>
2026-09-07 18:24:43 +0800

    清理内核构建中间文件

--- kernel/modules/.hwrun_core.mod.cmd
+++
@@ -1,1 +0,0 @@
-cmd_/mnt/c/Users/xingg/Desktop/hwrun-os-linux/kernel/modules/hwrun_core.mod := printf '%s\n'   hwrun_core.o | awk '!x[$$0]++ { print("/mnt/c/Users/xingg/Desktop/hwrun-os-linux/kernel/modules/"$$0) }' > /mnt/c/Users/xingg/Desktop/hwrun-os-linux/kernel/modules/hwrun_core.mod
--- kernel/modules/.modules.order.cmd
+++
@@ -1,1 +0,0 @@
-cmd_/mnt/c/Users/xingg/Desktop/hwrun-os-linux/kernel/modules/modules.order := {   echo /mnt/c/Users/xingg/Desktop/hwrun-os-linux/kernel/modules/hwrun_core.ko; :; } > /mnt/c/Users/xingg/Desktop/hwrun-os-linux/kernel/modules/modules.order
